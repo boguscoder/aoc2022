bde3b682dfff55ce244ae03f859bd14c59ccd4fe
Oleg Bogdanov <boguscoder@fb.com>
2022-12-05 13:52:09 -0800

    day 3.1

---
+++ day3/src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+use std::collections::HashSet;
+
+fn common_item_prio(line: &str) -> i32 {
+  let (split1, split2) = line.split_at(line.chars().count() / 2);
+  let ruck1: HashSet<char> = HashSet::from_iter(split1.chars());
+  let ruck2: HashSet<char> = HashSet::from_iter(split2.chars());
+
+  let common = ruck1.intersection(&ruck2).next().unwrap();
+
+  return match common {
+    'a'..='z' => *common as i32 - 97 +1,
+    'A'..='Z' => *common as i32 - 65 + 27,
+    _ => panic!("wrong input")
+  };
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let contents = fs::read_to_string(&args[2])
+    .expect("Should have been able to read the file");
+
+  {
+    // round 1
+    let sum : i32 = contents.split("\n").map(common_item_prio).sum();
+    println!("{}", sum);
+  }
+}

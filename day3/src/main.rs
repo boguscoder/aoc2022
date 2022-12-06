162356ab606661245fa0024a0db2e3a590c4a895
Oleg Bogdanov <boguscoder@gmail.com>
2022-12-05 21:19:32 -0800

    day3.2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,19 +1,35 @@
+#![feature(iter_array_chunks)]
+
 use std::env;
 use std::fs;
 use std::collections::HashSet;
 
-fn common_item_prio(line: &str) -> i32 {
+fn prio(item: char) -> i32 {
+  match item {
+    'a'..='z' => item as i32 - 97 +1,
+    'A'..='Z' => item as i32 - 65 + 27,
+    _ => panic!("wrong input")
+  }
+}
+
+fn common_per_line(line: &str) -> i32 {
   let (split1, split2) = line.split_at(line.chars().count() / 2);
   let ruck1: HashSet<char> = HashSet::from_iter(split1.chars());
   let ruck2: HashSet<char> = HashSet::from_iter(split2.chars());
 
-  let common = ruck1.intersection(&ruck2).next().unwrap();
+  let common = *ruck1.intersection(&ruck2).next().unwrap();
+  prio(common)
+}
 
-  match common {
-    'a'..='z' => *common as i32 - 97 +1,
-    'A'..='Z' => *common as i32 - 65 + 27,
-    _ => panic!("wrong input")
-  }
+fn common_per_trio(trio: [&str; 3]) -> i32 {
+  let ruck1: HashSet<char> = HashSet::from_iter(trio[0].chars());
+  let ruck2: HashSet<char> = HashSet::from_iter(trio[1].chars());
+  let ruck3: HashSet<char> = HashSet::from_iter(trio[2].chars());
+
+  let mut ruck1and2 = ruck1.intersection(&ruck2);
+  let common = ruck1and2.find(|e| ruck3.contains(e)).unwrap();
+
+  prio(*common)
 }
 
 fn main() {
@@ -23,7 +39,13 @@ fn main() {
 
   {
     // round 1
-    let sum : i32 = contents.split('\n').map(common_item_prio).sum();
-    println!("{}", sum);
+    let sum : i32 = contents.split('\n').map(common_per_line).sum();
+    println!("{sum}");
+  }
+
+  {
+    // round 2
+    let sum : i32 = contents.split('\n').array_chunks().map(common_per_trio).sum();
+    println!("{sum}");
   }
 }
